e66283f255622da0e210aa3878f3998f2d3929c8
JingsongMeng <mengjs.92@gmail.com>
2024-01-12 14:18:19 -0500

    create remote user

--- hdfs_test.java
+++ hdfs_test.java
@@ -2,6 +2,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.commons.cli.*;
+import org.apache.hadoop.security.UserGroupInformation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -25,7 +26,7 @@ public class hdfs_test {
     private static final String HDFS_SITE_PATH_STR = "/users/jason92/local/hadoop-" + HADOOP_VERSION + "/etc/hadoop/hdfs-site.xml";
     private static final String YARN_SITE_PATH_STR = "/users/jason92/local/hadoop-" + HADOOP_VERSION + "/etc/hadoop/yarn-site.xml";
     private static final Configuration CONF = new Configuration();
-    private static final String HDFS_DIR = "/user/jason92/";
+    // private static final String HDFS_DIR = "/user/jason92/";
 
     private static class ThreadTask implements Runnable {
         private FileSystem fs;
@@ -33,40 +34,69 @@ public class hdfs_test {
         private int threadID;
         private int totalThread;
         private int numOfActions;
+        private UserGroupInformation ugi;
 
 
-        public ThreadTask (FileSystem fs, String dir, int threadID, int totalThread, int numOfActions) {
+
+        public ThreadTask (UserGroupInformation ugi, FileSystem fs, String dir, int threadID, int totalThread, int numOfActions) {
             this.fs = fs;
             this.dir = new Path(dir);
             this.threadID = threadID;
             this.totalThread = totalThread;
             this.numOfActions = numOfActions;
+            this.ugi = ugi;
         }
 
         @Override
         public void run() {
-            try {
-                // ThreadLocalRandom tlr = ThreadLocalRandom.current();
-                for (int i = 0; i < this.numOfActions; i++) {
-                    int fileID = i * this.totalThread + this.threadID;
-                    Path path = new Path(this.dir, "file-" + fileID);
-                    String content = String.valueOf(fileID);
-                    OutputStream os = this.fs.create(path, true);
-                    os.write(content.getBytes());
-                    if (i % 10 == 0) System.out.format("write %s to file %d%n", content, fileID);
-                    os.close();
-                    for (int j = i; j >=0 && j > i - 5; j--) {
-                        fileID = j * this.totalThread + this.threadID;
-                        path = new Path(this.dir, "file-" + fileID);
-                        InputStream is = fs.open(path);
-                        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                        content = br.readLine();
-                        if (i % 10 == 0) System.out.format("read %s from file %d%n", content, fileID);
-                        br.close();
-                        is.close();
-                    }
-                }
+            // try {
+            //     for (int i = 0; i < this.numOfActions; i++) {
+            //         int fileID = i * this.totalThread + this.threadID;
+            //         Path path = new Path(this.dir, "file-" + fileID);
+            //         String content = String.valueOf(fileID);
+            //         OutputStream os = this.fs.create(path, true);
+            //         os.write(content.getBytes());
+            //         if (i % 10 == 0) System.out.format("write %s to file %d%n", content, fileID);
+            //         os.close();
+            //         for (int j = i; j >=0 && j > i - 5; j--) {
+            //             fileID = j * this.totalThread + this.threadID;
+            //             path = new Path(this.dir, "file-" + fileID);
+            //             InputStream is = fs.open(path);
+            //             BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            //             content = br.readLine();
+            //             if (i % 10 == 0) System.out.format("read %s from file %d%n", content, fileID);
+            //             br.close();
+            //             is.close();
+            //         }
+            //     }
 
+            // } catch (Exception e) {
+            //     System.err.format("Exception in run(): %s", e.getMessage());
+            // }
+
+            try {
+                this.ugi.doAs((java.security.PrivilegedExceptionAction<Void>) () -> {
+                    for (int i = 0; i < this.numOfActions; i++) {
+                        int fileID = i * this.totalThread + this.threadID;
+                        Path path = new Path(this.dir, "file-" + fileID);
+                        String content = String.valueOf(fileID);
+                        OutputStream os = this.fs.create(path, true);
+                        os.write(content.getBytes());
+                        if (i % 10 == 0) System.out.format("write %s to file %d%n", content, fileID);
+                        os.close();
+                        for (int j = i; j >=0 && j > i - 5; j--) {
+                            fileID = j * this.totalThread + this.threadID;
+                            path = new Path(this.dir, "file-" + fileID);
+                            InputStream is = fs.open(path);
+                            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                            content = br.readLine();
+                            if (i % 10 == 0) System.out.format("read %s from file %d%n", content, fileID);
+                            br.close();
+                            is.close();
+                        }
+                    }
+                    return null;
+                });
             } catch (Exception e) {
                 System.err.format("Exception in run(): %s", e.getMessage());
             }
@@ -78,17 +108,21 @@ public class hdfs_test {
         CONF.addResource(new Path(HDFS_SITE_PATH_STR));
         CONF.addResource(new Path(YARN_SITE_PATH_STR));
 
-        int threadPoolSize = Integer.parseInt(args[0]);
-        int threadSize = Integer.parseInt(args[1]);
-        int numOfActions = Integer.parseInt(args[2]);
-
+        String username = args[0];
+        int threadPoolSize = Integer.parseInt(args[1]);
+        int threadSize = Integer.parseInt(args[2]);
+        int numOfActions = Integer.parseInt(args[3]);
+        String hdfs_dir = "/user/"+username;
+        
         try {
+            UserGroupInformation ugi = UserGroupInformation.createProxyUser(username, UserGroupInformation.getCurrentUser());
+
             FileSystem fs = FileSystem.get(CONF);
 
             ExecutorService executor = Executors.newFixedThreadPool(threadPoolSize);
 
             for (int i = 0; i < threadSize; i++) {
-                ThreadTask task = new ThreadTask(fs, HDFS_DIR, i, threadSize, numOfActions);
+                ThreadTask task = new ThreadTask(ugi, fs, hdfs_dir, i, threadSize, numOfActions);
                 executor.execute(task);
             }
 
