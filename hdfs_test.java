cf389f5b7ee4f0c9b637a495da8057df8eda5513
JingsongMeng <mengjs.92@gmail.com>
2024-02-14 01:47:00 -0500

    add ScheduledTask class

--- hdfs_test.java
+++ hdfs_test.java
@@ -5,6 +5,7 @@ import org.apache.commons.cli.*;
 import org.apache.hadoop.security.UserGroupInformation;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -12,11 +13,14 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.io.BufferedOutputStream;
 import java.io.OutputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 
 
@@ -30,8 +34,8 @@ public class hdfs_test {
         String hdfs_site_path = "/users/jason92/hadoop-" + hadoop_version + "/etc/hadoop/hdfs-site.xml";
         String yarn_site_path = "/users/jason92/hadoop-" + hadoop_version + "/etc/hadoop/yarn-site.xml";
         String username = args[1];
-        int threadSize = Integer.parseInt(args[2]);
-	    int dataSize = Integer.parseInt(args[3]);
+	    int dataSize = Integer.parseInt(args[2]);
+        int threadSize = Integer.parseInt(args[3]);
         String hdfs_dir = "/user/"+username;
 
         CONF.addResource(new Path(core_site_path));
@@ -40,42 +44,23 @@ public class hdfs_test {
         
         
         try {
-
+            ScheduledExecutorService executor = Executors.newScheduledThreadPool(threadSize);
             FileSystem fs = FileSystem.get(CONF);
+            byte[] data = new byte[dataSize];
 
             for (int i = 0; i < threadSize; i++) {
-                int ii = i;
-		        int sz = dataSize;
-		        byte[] data = new byte[dataSize];
-                try {
-                    Path path = new Path(hdfs_dir, "file-" + ii);
-                    OutputStream os = fs.create(path, true);
-                    Thread closeThread = new Thread(() -> {
-                        try {
-                            Thread.sleep(500);
-                            System.out.println("closeThread waked up");
-                            os.close();
-                            System.out.println("os closed");
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    });
-                    long start = System.nanoTime();
-                    closeThread.start();
-                    System.out.println("ready");
-                    Thread.sleep(5000);
-                    System.out.println("go");
-                    os.write(data);
-
-                    long end = System.nanoTime();
-                    long ms = (end - start) / 1000000;
-                    System.out.format("file %d: write %d bytes to hdfs took %d ms%n", ii, sz, ms);
-                    os.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                Path path = new Path(hdfs_dir, "file-" + i);
+                ScheduledTask task = new ScheduledTask(fs, data, path);
+                executor.schedule(task, 2, TimeUnit.SECONDS);
             }
 
+            // ScheduledTask killer = new ScheduledTask(1000);
+            // executor.schedule(killer, 2, TimeUnit.SECONDS);
+
+            executor.shutdown();
+            executor.awaitTermination(60, TimeUnit.SECONDS);
+            executor.shutdownNow();
+            System.out.println("executor shutdown");
             fs.close();
 
         } catch (Exception e) {
@@ -84,4 +69,66 @@ public class hdfs_test {
 
         
     }
+
+    public static class ScheduledTask implements Runnable {
+        static AtomicInteger GlobalID = new AtomicInteger(0);
+        int id;
+        boolean killer;
+        int waitMS = 0;
+        FileSystem fs;
+        byte[] data;
+        Path path;
+        OutputStream os;
+        DateTimeFormatter formatter;
+        public ScheduledTask(FileSystem fs, byte[] data, Path path) {
+            try{
+                id = GlobalID.getAndIncrement();
+                killer = false;
+                this.fs = fs;
+                this.data = data;
+                this.path = path;
+                os = fs.create(path, true);
+                formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSSSSS");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public ScheduledTask(int waitMS) {
+            // id = GlobalID.getAndIncrement();
+            killer = true;
+            this.waitMS = waitMS;
+            formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSSSSSSSS");
+        }
+
+        @Override
+        public void run() {
+            try {
+                if (killer) {
+                    Thread.sleep(waitMS);
+                    LocalTime exitTime = LocalTime.now();
+                    String formattedExitTime = exitTime.format(formatter);
+                    System.out.format("exit at [%s]%n", formattedExitTime);
+                    System.exit(0);
+                    return;
+                }
+
+                
+                long start = System.nanoTime();
+                os.write(data);
+                long end = System.nanoTime();
+
+                LocalTime startTime = LocalTime.ofNanoOfDay(TimeUnit.NANOSECONDS.toNanos(start));
+                String formattedStartTime = startTime.format(formatter);
+                double elapsedTime = (end - start) / 1000000.0;
+                System.out.format("%d: start at [%s], write %d bytes, took %f ms %n", id, formattedStartTime, data.length, elapsedTime);
+                os.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
+
+
